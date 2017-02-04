f2974e81173226e28e0715c1d208714d0619503c
Brian Peiris <brianpeiris@gmail.com>
2017-02-03 16:52:10 -0800

    initial commit

---
+++ query-configurable.js
@@ -0,0 +1,14 @@
+AFRAME.registerComponent('query-configurable', {
+	init: function () {
+		this.el.addEventListener('loaded', this.applyQueryParams.bind(this));
+	},
+	applyQueryParams: function () {
+		var params = (location.search || '?').substring(1).split('&');
+		params.forEach(function (param) {
+			var parts = param.split('=');
+			var name = parts[0];
+			var val = parts[1];
+			this.el.setAttribute(name, val);
+		}.bind(this));
+	}
+});
